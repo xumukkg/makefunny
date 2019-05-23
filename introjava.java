cb6a2e09dc770b9e6cb9bbcd05078d5afd578a0f
Nursultan <47150499+xumukkg@users.noreply.github.com>
2019-05-23 16:33:11 +0600

    Add files via upload

---
+++ introjava.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class introjava {
+
+
+        // Define your tree class in this file!
+        public static void main(String[] args) {
+            // hello
+            System.out.println("Hey there, Im Ariel and Im learning to code in Java!");
+            System.out.println("Im going to plant a tree today!");
+            System.out.println("    *");
+            System.out.println("   *** ");
+            System.out.println("  ***** ");
+            System.out.println(" ******* ");
+            System.out.println(" ******* ");
+            System.out.println("  ***** ");
+            System.out.println("   ***  ");
+            System.out.println("   ***  ");
+
+            System.out.println("    o,,,o  ");
+            System.out.println("  ( ' ; ' ) ");
+            System.out.println("  (   ,   )" );
+            System.out.println("  (,,) (,,)");
+            System.out.println("")  ;
+
+
+
+        }
+
+}
